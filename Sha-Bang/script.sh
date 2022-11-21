ffe6a68971538dfedde5b092424a539c706d5a63
Issa Mboup <issa.mboup314@gmail.com>
2022-11-21 11:29:30 +0100

    exercise

---
+++ Sha-Bang/script.sh
@@ -0,0 +1,7 @@
+#!bin/zsh
+touch log.log
+echo "Today day is `date + %j` >> log.log"
+echo "The current logged users are:  >> log.log"
+who |  awk '{print $1}  >> log.log'
+echo "Your system is up since: `uptime`  >> log.log"
+
